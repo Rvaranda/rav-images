c49e34c0215a65acfe1045bb4c6140d69a4a5fc7
Rvaranda <rafael_andrademiranda@yahoo.com.br>
2023-04-09 17:07:36 -0300

    Rendering saved images in main page

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,9 +1,10 @@
-function Card() {
+function Card({ src }) {
   return (
     <div className="group relative max-w-xs overflow-hidden rounded-md shadow-md">
       <img
         className="object-fit h-80 w-80"
-        src="http://via.placeholder.com/320x320"
+        // src="http://via.placeholder.com/320x320"
+        src={src}
         alt="image"
       />
       <div className="absolute bottom-0 hidden bg-black/50 p-4 text-white group-hover:block">
--- client/src/components/RenderCards.jsx
+++ client/src/components/RenderCards.jsx
@@ -1,14 +1,27 @@
+import { useState, useEffect } from "react";
 import Card from "./Card";
 
 function RenderCards() {
+  const [imagesUrls, setImagesUrls] = useState([]);
+
+  useEffect(() => {
+    const fetchImageUrls = async () => {
+      try {
+        const response = await fetch("http://localhost:8080/api/images");
+        const data = await response.json();
+        setImagesUrls(data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchImageUrls();
+  }, []);
+
   return (
     <div className="grid grid-cols-1 place-items-center gap-4 p-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5">
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+      {imagesUrls.map((image) => (
+        <Card key={image} src={image} />
+      ))}
     </div>
   );
 }
