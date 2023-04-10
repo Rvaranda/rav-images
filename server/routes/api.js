f7b7cc5d412dccf7b93209ab1f63483976426c63
Rvaranda <rafael_andrademiranda@yahoo.com.br>
2023-04-09 21:38:14 -0300

    Unique image naming and url storing in database

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,7 +1,8 @@
+const { v4: uuidv4 } = require("uuid");
 const express = require("express");
 const openai = require("../config/openai");
 const Post = require("../models/Post");
-const { S3Client, PutObjectCommand, ListObjectsCommand } = require("@aws-sdk/client-s3");
+const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
 
 const router = express.Router();
 
@@ -24,25 +25,7 @@ router.get("/image", async (req, res) => {
 });
 
 router.get("/images", async (req, res) => {
-  const s3 = new S3Client({ region: "sa-east-1" });
-
-  const command = new ListObjectsCommand({
-    Bucket: "rav-images"
-  });
-  
-  try {
-    const response = await s3.send(command);
-    console.log(response);
-    const urls = response.Contents.map(e => {
-      let key = e.Key.replace(/\s/g, "+");
-      return `https://rav-images.s3.sa-east-1.amazonaws.com/${key}`;
-    });
-    res.status(200).json(urls);
-  }
-  catch (err) {
-    console.log(err);
-    res.status(500).json({ message: err.message });
-  }
+  res.send("hello");
 });
 
 router.post("/image/submit", async (req, res) => {
@@ -53,9 +36,12 @@ router.post("/image/submit", async (req, res) => {
 
   const s3 = new S3Client({ region: "sa-east-1" });
 
+  const key = `RAV_${uuidv4()}_${new Date().getTime()}.png`;
+  const imageUrl = `https://${process.env.BUCKET_NAME}.s3.${process.env.REGION}.amazonaws.com/${key}`;
+  
   const command = new PutObjectCommand({
-    Bucket: "rav-images",
-    Key: `${user}-${prompt}.png`,
+    Bucket: process.env.BUCKET_NAME,
+    Key: key,
     Body: Buffer.from(imageData, "base64"),
     ContentEncoding: "base64",
     ContentType: "image/png"
@@ -65,8 +51,8 @@ router.post("/image/submit", async (req, res) => {
     const response = await s3.send(command);
     console.log(response);
 
-    // const post = await Post.create({ user, prompt, image: 'image' });
-    res.status(201).json({message: "ok"});
+    const post = await Post.create({ user, prompt, image: imageUrl });
+    res.status(201).json(post);
   }
   catch (err) {
     console.log(err);
