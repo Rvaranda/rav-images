b99749b64b8d531e1398f8d44838b9d56bbad49b
Rvaranda <rafael_andrademiranda@yahoo.com.br>
2023-04-12 15:16:00 -0300

    Improved the card posts

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,14 +1,15 @@
+import { FaRegUserCircle } from "react-icons/fa";
+
 function Card({ post }) {
   return (
     <div className="group relative max-w-xs overflow-hidden rounded-md shadow-md">
-      <img
-        className="object-fit h-80 w-80"
-        // src="http://via.placeholder.com/320x320"
-        src={post.image}
-        alt="image"
-      />
-      <div className="absolute bottom-0 hidden bg-black/50 p-4 text-white group-hover:block">
-        <p>{post.prompt}</p>
+      <img className="object-fit h-80 w-80" src={post.image} alt="image" />
+      <div className="absolute bottom-0 hidden w-full bg-black/70 p-4 text-white group-hover:block">
+        <p className="mb-4">{post.prompt}</p>
+        <div className="flex items-center gap-1">
+          <FaRegUserCircle />
+          <p className="text-xs">{post.user}</p>
+        </div>
       </div>
     </div>
   );
