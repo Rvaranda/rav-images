e7cd98f78f34056460d63c0e51f3f6c689b0f34b
Rvaranda <rafael_andrademiranda@yahoo.com.br>
2023-04-14 00:07:41 -0300

    Changed the Create page layout

--- client/src/components/FormInput.jsx
+++ client/src/components/FormInput.jsx
@@ -1,7 +1,7 @@
-function FormInput({ id, title, type, value, setValue }) {
+function FormInput({ className, id, title, type, value, setValue }) {
   return (
-    <>
-      <label className="mb-4 ml-4 block" htmlFor={id}>
+    <div className="w-full">
+      <label className="mb-2 ml-2 block" htmlFor={id}>
         {title}
       </label>
       <input
@@ -9,10 +9,11 @@ function FormInput({ id, title, type, value, setValue }) {
         id={id}
         value={value}
         onChange={(e) => setValue(e.target.value)}
-        className="block w-1/2 rounded-full border-2 border-slate-300 px-4 py-2 outline-none focus:border-slate-500"
+        autoComplete="off"
+        className={`${className} block w-full rounded-full border-2 border-slate-300 px-4 py-2 outline-none focus:border-slate-500`}
         required
       />
-    </>
+    </div>
   );
 }
 
--- client/src/pages/Create.jsx
+++ client/src/pages/Create.jsx
@@ -9,23 +9,23 @@ function Create() {
   const [previewImg, setPreviewImg] = useState(null);
   const [loading, setLoading] = useState(false);
 
+  const [promptError, setPromptError] = useState(false);
+
   async function generateImage() {
     if (!prompt) {
-      console.log("debug");
+      setPromptError(true);
       return;
     }
     setLoading(true);
+    setPromptError(false);
 
     try {
-      const response = await fetch(
-        `http://localhost:8080/api/image?prompt=${prompt}`,
-        {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
+      const response = await fetch(`http://localhost:8080/api/image?prompt=${prompt}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
 
       const data = await response.json();
       setPreviewImg(data.data[0].b64_json);
@@ -58,56 +58,59 @@ function Create() {
   }
 
   return (
-    <section className="p-16">
-      <h1 className="mb-16 text-4xl font-bold">
-        Create awesome images in an instant
-      </h1>
-      <form>
-        <FormInput
-          id="name"
-          title="Your name"
-          type="text"
-          value={name}
-          setValue={setName}
-        />
-        <FormInput
-          id="prompt"
-          title="Prompt"
-          type="text"
-          value={prompt}
-          setValue={setPrompt}
-        />
-      </form>
-      <div className="relative mt-8">
-        <img
-          className="rounded-lg object-contain"
-          src={
-            previewImg
-              ? `data:image/png;base64,${previewImg}`
-              : "http://via.placeholder.com/500x500"
-          }
-          alt="preview"
-          width={500}
-          height={500}
-        />
-        {loading && <Loading />}
+    <section className="">
+      <div className="flex h-96 items-center justify-center bg-gradient-to-tl from-yellow-300 to-blue-500">
+        <h1 className="text-4xl font-bold">Create awesome images in an instant</h1>
       </div>
-      <button
-        className="mt-2 block w-[500px] rounded-md bg-green-600 p-2 text-lg text-white hover:bg-green-800"
-        type="button"
-        onClick={generateImage}
-        disabled={loading}
-      >
-        {loading ? "Generating..." : "Generate"}
-      </button>
-      <button
-        className="mt-2 block w-[500px] rounded-md bg-blue-600 p-2 text-lg text-white hover:bg-blue-800"
-        type="button"
-        onClick={createPost}
-        disabled={!previewImg}
-      >
-        Submit
-      </button>
+      <div className="mt-8 flex justify-between gap-16 px-16">
+        <div className="relative h-[600px] w-[600px]">
+          <img
+            className="rounded-lg object-contain"
+            src={
+              previewImg
+                ? `data:image/png;base64,${previewImg}`
+                : "http://via.placeholder.com/600x600"
+            }
+            alt="preview"
+            width={600}
+            height={600}
+          />
+          {loading && <Loading />}
+        </div>
+        <div className="flex grow flex-col">
+          <div className="mb-16 flex flex-col items-end gap-4">
+            <FormInput
+              className={`${promptError ? "border-red-600 focus:border-red-600" : ""}`}
+              id="prompt"
+              title="Enter your prompt"
+              type="text"
+              value={prompt}
+              setValue={setPrompt}
+            />
+
+            <button
+              className="w-36 rounded-md bg-green-600 py-2 text-lg text-white hover:bg-green-800 disabled:bg-green-300"
+              type="button"
+              onClick={generateImage}
+              disabled={loading}
+            >
+              {loading ? "Generating..." : "Generate"}
+            </button>
+          </div>
+          <div className="flex w-1/2 max-w-md flex-col items-end gap-4 self-end">
+            <h3 className="w-full text-center text-2xl font-bold">Want to share your image?</h3>
+            <FormInput id="name" title="Your name" type="text" value={name} setValue={setName} />
+            <button
+              className="w-36 rounded-md bg-blue-600 py-2 text-lg text-white hover:bg-blue-800 disabled:bg-neutral-400"
+              type="button"
+              onClick={createPost}
+              disabled={!previewImg || !name}
+            >
+              Share
+            </button>
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
