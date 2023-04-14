967ef781415c55098c493b04f5d9bd156232586a
Rvaranda <rafael_andrademiranda@yahoo.com.br>
2023-04-14 00:07:51 -0300

    Added Footer

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer className="mt-16 bg-slate-400 p-8 text-center">
+      RAV Images &copy; Rafael Andrade Varanda
+    </footer>
+  );
+}
+
+export default Footer;
