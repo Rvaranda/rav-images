39aecb7a4e4a767a65ac431a09ba65192ab30d76
Rvaranda <rafael_andrademiranda@yahoo.com.br>
2023-04-19 14:51:14 -0300

    Responsive Create page

--- client/src/components/Loading.jsx
+++ client/src/components/Loading.jsx
@@ -1,6 +1,6 @@
 function Loading() {
   return (
-    <div className="absolute top-0 flex h-[600px] w-[600px] items-center justify-center bg-black bg-opacity-40">
+    <div className="absolute top-0 flex h-full w-full items-center justify-center rounded-lg bg-black bg-opacity-40">
       <div className="h-12 w-12 animate-spin rounded-full border-[6px] border-gray-400 border-r-black"></div>
     </div>
   );
--- client/src/pages/Create.jsx
+++ client/src/pages/Create.jsx
@@ -74,22 +74,24 @@ function Create() {
   return (
     <section className="">
       <div className="flex h-96 items-center justify-center bg-gradient-to-tl from-yellow-300 to-blue-500">
-        <h1 className="text-4xl font-bold">Create awesome images in an instant</h1>
+        <h1 className="p-4 text-center text-4xl font-bold">Create awesome images in an instant</h1>
       </div>
-      <div className="mt-8 flex justify-between gap-16 px-16">
-        <div className="relative">
-          <img
-            className="rounded-lg object-contain"
-            src={
-              previewImg
-                ? `data:image/png;base64,${previewImg}`
-                : "http://via.placeholder.com/600x600"
-            }
-            alt="preview"
-            width={600}
-            height={600}
-          />
-          {loading && <Loading />}
+      <div className="mt-8 grid gap-4 px-8 lg:grid-cols-2 lg:grid-rows-2">
+        <div className="order-2 row-span-2 justify-self-center lg:order-none">
+          <div className="relative">
+            <img
+              className="rounded-lg object-contain"
+              src={
+                previewImg
+                  ? `data:image/png;base64,${previewImg}`
+                  : "http://via.placeholder.com/600x600"
+              }
+              alt="preview"
+              width={600}
+              height={600}
+            />
+            {loading && <Loading />}
+          </div>
           <button
             type="button"
             className="mt-4 block w-full rounded-md bg-black px-4 py-2 text-xl text-white disabled:bg-neutral-400"
@@ -99,42 +101,40 @@ function Create() {
             Download image
           </button>
         </div>
-        <div className="flex grow flex-col">
-          <div className="mb-16 flex flex-col items-end gap-4">
-            <FormInput
-              className={`${
-                promptError
-                  ? "border-red-600 focus:border-red-600"
-                  : "border-slate-300 focus:border-slate-500"
-              }`}
-              id="prompt"
-              title="Enter your prompt"
-              type="text"
-              value={prompt}
-              setValue={setPrompt}
-            />
+        <div className="order-1 mb-8 flex flex-col items-end gap-4 lg:order-none">
+          <FormInput
+            className={`${
+              promptError
+                ? "border-red-600 focus:border-red-600"
+                : "border-slate-300 focus:border-slate-500"
+            }`}
+            id="prompt"
+            title="Enter your prompt"
+            type="text"
+            value={prompt}
+            setValue={setPrompt}
+          />
 
-            <button
-              className="w-36 rounded-md bg-green-600 py-2 text-lg text-white hover:bg-green-800 disabled:bg-green-300"
-              type="button"
-              onClick={generateImage}
-              disabled={loading}
-            >
-              {loading ? "Generating..." : "Generate"}
-            </button>
-          </div>
-          <div className="flex w-1/2 max-w-md flex-col items-end gap-4 self-end">
-            <h3 className="w-full text-center text-2xl font-bold">Want to share your image?</h3>
-            <FormInput id="name" title="Your name" type="text" value={name} setValue={setName} />
-            <button
-              className="w-36 rounded-md bg-blue-600 py-2 text-lg text-white hover:bg-blue-800 disabled:bg-neutral-400"
-              type="button"
-              onClick={createPost}
-              disabled={!previewImg || !name}
-            >
-              Share
-            </button>
-          </div>
+          <button
+            className="w-full rounded-md bg-green-600 py-2 text-lg text-white hover:bg-green-800 disabled:bg-green-300 sm:w-36"
+            type="button"
+            onClick={generateImage}
+            disabled={loading}
+          >
+            {loading ? "Generating..." : "Generate"}
+          </button>
+        </div>
+        <div className="order-3 mt-8 flex w-full max-w-2xl flex-col gap-4 sm:justify-self-center lg:order-none xl:justify-self-end">
+          <h3 className="text-center text-2xl font-bold">Want to share your image?</h3>
+          <FormInput id="name" title="Your name" type="text" value={name} setValue={setName} />
+          <button
+            className="rounded-md bg-blue-600 py-2 text-lg text-white hover:bg-blue-800 disabled:bg-neutral-400 sm:w-36 sm:self-end"
+            type="button"
+            onClick={createPost}
+            disabled={!previewImg || !name}
+          >
+            Share
+          </button>
         </div>
       </div>
     </section>
