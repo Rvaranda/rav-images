12bf9526bebc3bc7072a7809cfb7699b8335e3c9
Rvaranda <rafael_andrademiranda@yahoo.com.br>
2023-04-19 22:48:42 -0300

    Improved responsive behavior of posts cards

--- client/src/components/RenderCards.jsx
+++ client/src/components/RenderCards.jsx
@@ -2,7 +2,7 @@ import Card from "./Card";
 
 function RenderCards({ posts }) {
   return (
-    <div className="grid grid-cols-1 place-items-center gap-4 p-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5">
+    <div className="grid grid-cols-1 place-items-center gap-4 p-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
       {posts.map((post) => (
         <Card key={post._id} post={post} />
       ))}
