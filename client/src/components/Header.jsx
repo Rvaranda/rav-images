409cfb0483d5f2908fb2c12c4559082e75058140
Rvaranda <rafael_andrademiranda@yahoo.com.br>
2023-04-20 09:29:53 -0300

    Improved Header responsiveness

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,16 +3,15 @@ import { Link } from "react-router-dom";
 function Header() {
   return (
     <header className="p-4">
-      <section className="m-auto flex max-w-7xl items-center justify-between">
+      <section className="m-auto flex max-w-7xl flex-col items-center gap-4 sm:flex-row sm:items-center sm:justify-between">
         <div>
           <Link to="/">
-            {/* <img src="http://via.placeholder.com/100x50" alt="logo" /> */}
             <p className="text-3xl font-bold text-slate-500">RAV Images</p>
           </Link>
         </div>
         <Link
           to="/create"
-          className="rounded-md bg-blue-400 px-4 py-2 text-xl text-white hover:bg-blue-600"
+          className="w-full max-w-xs rounded-md bg-blue-400 px-4 py-2 text-center text-xl text-white hover:bg-blue-600 sm:w-auto"
         >
           Create
         </Link>
